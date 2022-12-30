43012a11bcf8eed87f2038e5f5c14eeac90dedcc
Daniel Woelk <64069335+DanielPL56@users.noreply.github.com>
2022-12-30 16:05:27 +0100

    Delete testing console
    This shouldn't be here

--- TestingConsole/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using TestingConsole;
-
-Dog dog = new Dog();
-dog.DateOfBirth = DateTime.Now;
-
-Console.WriteLine(dog.DateOfFirstDeWorming.ToString());
-Console.WriteLine(dog.DateOfSecondDeWorming.ToString());
-Console.WriteLine(dog.DateOfThirdDeWorming.ToString());
-Console.WriteLine(dog.DateOfFirstVaccination.ToString());
